f59cce40e364210619f881b908f44f190a810eda
yuanhang <itlodge.net@gmail.com>
2013-04-29 15:24:56 +0800

    update

---
+++ .emacs.d/coding.el
@@ -0,0 +1,194 @@
+;;; coding.el --- configuration file for programming
+;;
+;; Copyright (c) 2013 yuanhang zheng
+;;
+;; Author: yuanhang zheng <zhengyhn@gmail.com>
+;;
+
+;;; code
+
+;;require
+;(load-file (concat plugins-path "dash.el"))
+(require 'dash)
+
+;; ido-mode
+(require 'ido)
+(ido-mode t)
+
+;;comment
+(global-set-key (kbd "M-;") 'comment-or-uncomment-region)
+(global-set-key (kbd "M-'") 'comment-box) 
+
+;code style
+;(load-file (concat plugins-path "guess-offset.el"))
+(require 'guess-offset)
+;(load-file (concat plugins-path "smart-tab.el"))
+
+;c
+(eval-after-load 'cc-mode
+  '(progn
+	 (setq c-default-style
+		'((c-mode . "k&r")))
+	 ;;cscope
+;	 (load-file (concat plugins-path "xcscope.el"))
+	 (require 'xcscope)
+	 (global-set-key (kbd "C-'") 'cscope-find-global-definition-no-prompting)
+	 (global-set-key (kbd "C-;") 'cscope-pop-mark)
+	 (setq cscope-display-cscope-buffer nil)))
+
+;pascal
+;(load-file (concat plugins-path "pascal.el"))
+;(require 'pascal)
+;(autoload 'pascal-mode "pascal" "Pascal Mode." t)
+;======================================================
+
+;;php-mode
+;(load-file (concat plugins-path "php-mode.el"))
+(autoload 'php-mode "php-mode")
+(add-to-list 'auto-mode-alist '("\\.php\\'" . php-mode))
+
+;;css-mode
+(eval-after-load 'css-mode
+  '(progn
+;	 (load-file (concat plugins-path "rainbow-mode.el"))
+	 (require 'rainbow-mode)
+
+	 (defun css-mode-defaults ()
+	   (setq css-indent-offset 2)
+	   (rainbow-mode +1))
+	 (setq my-css-mode-hook 'css-mode-defaults)
+	 (add-hook 'css-mode-hook (lambda ()
+								(run-hooks 'my-css-mode-hook)))))
+
+;;haskell
+(add-to-list 'load-path (concat plugins-path "haskell-mode"))
+(autoload 'haskell-mode "haskell-mode")
+(add-to-list 'auto-mode-alist '("\\.hs\\'" . haskell-mode))
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+;(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
+
+(autoload 'lua-mode "lua-mode")
+(add-to-list 'auto-mode-alist '("\\.lua\\'" . lua-mode))
+	     
+;smart complie
+(autoload 'smart-compile "~/.emacs.d/plugins/smart-compile.el")
+(global-set-key [f9] 'smart-compile)
+(global-set-key [f10] 'gdb)
+
+;auto-complete
+(add-to-list 'load-path "~/.emacs.d/plugins/auto-complete")
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories
+			 "~/.emacs.d/plugins/auto-complete/ac-dict")
+(ac-config-default)
+(setq ac-modes
+	  (append ac-modes '(org-mode)
+			  '(ielm)))
+
+;;srSpeedbar
+;(load-file (concat plugins-path "sr-speedbar.el"))
+(autoload 'sr-speedbar-toggle "sr-speedbar")
+(global-set-key (kbd "C-c b") 'sr-speedbar-toggle)
+
+;;yaSnippet
+(add-to-list 'load-path (concat plugins-path "yasnippet"))
+(require 'yasnippet)
+(yas-global-mode 1)
+;;key
+;=====text
+;email
+;user
+;=====c-mode
+;once:#ifndef
+;main
+;inc:#include
+;p:printf
+
+(defun insert-empty-line ()
+  "insert an empty line after the current line"
+  (interactive)
+  (move-end-of-line nil)
+  (open-line 1)
+  (forward-line 1)
+  (indent-according-to-mode))
+(global-set-key [(shift return)] 'insert-empty-line)
+
+(defun move-line-up ()
+  "move up the current line"
+  (interactive)
+  (transpose-lines 1)
+  (forward-line -2))
+(global-set-key (kbd "M-P") 'move-line-up)
+
+(defun move-line-down ()
+  "move down the current line"
+  (interactive)
+  (forward-line 1)
+  (transpose-lines 1)
+  (forward-line -1))
+(global-set-key (kbd "M-N") 'move-line-down)
+
+(defun indent-region-or-buffer ()
+  "indent a region if selected or the whole buffer"
+  (interactive)
+  (save-excursion
+	(if (region-active-p)
+		(progn
+		  (indent-region (region-beginning) (region-end))
+		  (message "the selectedd region has been indented!"))
+	  (progn
+		(indent-region (point-min) (point-max))
+		(message "the whole buffer has been indented!")))))
+(global-set-key (kbd "C-M-\\") 'indent-region-or-buffer)
+
+(defun duplicate-current-line-or-region (count)
+  "duplicates the current line or region COUNT times"
+  (interactive "p")
+  (let (beg end (origin (point)))
+	(if (and mark-active (> (point) (mark)))
+		(exchange-point-and-mark))
+	(setq beg (line-beginning-position))
+	(if mark-active
+		(exchange-point-and-mark))
+	(setq end (line-end-position))
+	(let ((region (buffer-substring-no-properties beg end)))
+	  (-dotimes count
+				(lambda (n)
+				  (goto-char end)
+				  (newline)
+				  (insert region)
+				  (setq end (point))))
+	  (goto-char (+ origin (* (length region) count) count)))))
+(global-set-key (kbd "M-d") 'duplicate-current-line-or-region)
+
+(defun rename-file-and-buffer ()
+  "renames the current buffer and it's file"
+  (interactive)
+  (let ((name (buffer-name))
+		(filename (buffer-file-name)))
+	(if (not (and filename (file-exists-p filename)))
+		(message "buffer '%s' is not visiting a file" name)
+	  (let ((new-name (read-file-name "new name: " filename)))
+		(cond ((get-buffer new-name)
+			   (message "a buffer named '%s' exists!" new-name))
+			  (t
+			   (rename-file name new-name 1)
+			   (rename-buffer new-name)
+			   (set-visited-file-name new-name)
+			   (set-buffer-modified-p nil)))))))
+(global-set-key (kbd "C-c r") 'rename-file-and-buffer)
+
+(defun delete-file-and-buffer ()
+  "kills the current buffer and deletes its file"
+  (interactive)
+  (let ((filename (buffer-file-name)))
+	(when filename
+	  (delete-file filename)
+	  (message "file '%s' has been deleted!" filename)))
+  (kill-buffer)) 
+(global-set-key (kbd "C-c DEL") 'delete-file-and-buffer)
+
+
+;;; coding.el ends here
---
+++ .emacs.d/life.el
@@ -0,0 +1,138 @@
+;;; life.el --- configuration file for beautiful life
+;;
+;; Copyright (c) 2013 yuanhang zheng
+;;
+;; Author: yuanhang zheng <zhengyhn@gmail.com>
+;;
+
+;;; code
+
+;;for goagent
+					;(setq url-proxy-services '(("http" . "127.0.0.1:8087")))
+
+;;org-mode
+(add-to-list 'load-path (concat plugins-path "org-mode/lisp"))
+(add-to-list 'load-path (concat plugins-path "org-mode/contrib/lisp") t)
+(eval-after-load "org-mode"
+  '(progn
+					; key bindings
+     (global-set-key (kbd "S-TAB") 'pcomplete)
+					; todo list
+     (setq org-todo-keywords
+	   '((sequence "TODO(t)" "DOING(i!)" "DONE(d!)")))
+					; code language
+     (org-babel-do-load-languages
+      'org-babel-load-languages
+      '((C . t)
+	(C++ . t)
+	(java . t)
+	(sh . t)
+	(js . t)
+	(sql . t)
+	(haskell . t)
+	(python . t)))
+     (setq org-export-default-language "zh-CN")
+     (add-hook 'org-mode-hook
+	       (lambda ()
+		 (setq truncate-liness nil)))))
+
+;;weibo
+(add-to-list 'load-path (concat plugins-path "weibo"))
+(autoload 'weibo-timeline "weibo")
+(global-set-key (kbd "C-c w") 'weibo-timeline)
+
+;;jabber-mode
+(add-to-list 'load-path (concat plugins-path "emacs-jabber-0.8.91"))
+(require 'jabber-autoloads)
+(global-set-key (kbd "C-c j c") 'jabber-connect-all)
+(eval-after-load "jabber-autoloads"
+  '(progn
+     (setq jabber-account-list
+	   '(("zhengyhn@gmail.com"
+	      (:password . " mvfcbuvemzqlcead ")
+	      (:network-server . "talk.google.com")
+	      (:connection-type . ssl))))
+     (global-set-key
+      (kbd "C-c j v") '(lambda ()
+			 "visit jabber roster"
+			 (interactive)
+			 (jabber-display-roster)
+			 (switch-to-buffer "*-jabber-roster-*")))))
+
+;;stardict
+					;(load-file (concat plugins-path "sdcv-mode.el"))
+(autoload 'sdcv-search "sdcv-mode")
+(global-set-key (kbd "C-c d") 'sdcv-search)
+
+;;emms
+(add-to-list 'load-path (concat plugins-path "emms-3.0"))
+(global-set-key (kbd "C-c e g") 'emms-play-directory)    ;go
+(autoload 'emms-play-directory "emms-setup")
+(autoload 'emms-play-directory "emms-player-mplayer")
+(eval-after-load "emms-setup"
+  '(progn
+     (setq exec-path (append exec-path '("/usr/bin")))
+     (emms-standard)
+     (emms-default-players)
+     (setq emms-player-list '(emms-player-mplayer)
+	   emms-player-mplayer-command-name "mplayer"
+	   emms-player-mplayer-parameters '("-slave"))
+     (setq emms-repeat-playlist t
+	   emms-source-file-default-directory "~/movie/music"
+	   emms-lyrics-dir "~/movie/music"
+	   emms-lyrics-coding-system nil
+	   emms-playlist-buffer-name "*EMMS*")
+     (global-set-key (kbd "C-c e v") 'emms-playlist-mode-go)  ;visit
+     (global-set-key (kbd "C-c e s") 'emms-stop)
+     (global-set-key (kbd "C-c e n") 'emms-next)
+     (global-set-key (kbd "C-c e p") 'emms-previous)
+     (global-set-key (kbd "C-c e r") 'emms-shuffle)           ;random
+     (global-set-key (kbd "C-c e SPC") 'emms-pause)
+     (global-set-key (kbd "C-c e d") 'emms-play-dired)
+     (global-set-key (kbd "C-c e x") 'emms-start)))
+
+;;google
+(defun google ()
+  "google a query or a region"
+  (interactive)
+  (browse-url
+   (concat
+    "http://www.google.com/search?ie=utf-8&oe=utf-8&q="
+    (url-hexify-string (if mark-active
+			   (buffer-substring (region-beginning) (region-end))
+			 (read-string "google: "))))))
+(global-set-key (kbd "C-c g") 'google)
+
+(add-to-list 'load-path (concat plugins-path "youdao-dict"))
+(autoload 'youdao-dict "youdao-dict")
+(global-set-key (kbd "C-c y") 'youdao-dict)
+
+;; douban fm
+(defun fmc-play ()
+  "if not start, start fmd and play, else, pause"
+  (interactive)
+  (if (equal (shell-command-to-string "ps -e | grep fmd") "")
+      (shell-command "~/git/fmd"))
+  (shell-command "~/script/fmc toggle"))
+(global-set-key (kbd "C-c m p") 'fmc-play)
+
+(defun fmc-next ()
+  "play next song"
+  (interactive)
+  (shell-command "~/script/fmc skip"))
+(global-set-key (kbd "C-c m n") 'fmc-next)
+
+(defun fmc-favor ()
+  "mark the current song as favor"
+  (interactive)
+  (shell-command "~/script/fmc rate"))
+(global-set-key (kbd "C-c m f") 'fmc-favor)
+
+(defun fmc-quit ()
+  "quit the douban fm client and daemon"
+  (interactive)
+  (shell-command "~/script/fmc end"))
+(global-set-key (kbd "C-c m q") 'fmc-quit)
+
+;;; life.el ends here
+
